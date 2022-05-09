f47adfb1fd79faed244055a4e1e424fcad216382
YoavOber <yoavoberko@gmail.com>
2022-05-09 17:06:46 +0300

    added token auth route. todo - refactor userschema to allow use of schema methods

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -1,24 +1,26 @@
 import DBResponse from "../models/DBResponse.model";
 import { User } from "../schemas/userSchema.mongo";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { IUser } from "../interfaces/IUser.interface";
 
 const getUserJWT = (user: IUser) => {
   const payload = {
-    username: user.username,
-    password: user.password,
-    email: user.email,
+    id: user.id,
   };
-  return sign(payload, process.env.JWT_SECRET || "");
+  return sign(payload, process.env.JWT_SECRET!);
+};
+
+const getPayload = (token: string) => {
+  const verified = <any>verify(token, process.env.JWT_SECRET!);
+  return verified.id;
 };
 
 //register
 //return jwt with user data if succuess
 //else return error message
-
 const register = async (username: string, email: string, password: string): Promise<DBResponse> => {
-  const emailExists = await User.find({ email: email });
-  if (emailExists) return new DBResponse(false, "מייל כבר קיים במערכת");
+  const emailExists = await User.findOne({ email: email });
+  if (emailExists != null) return new DBResponse(false, "מייל כבר קיים במערכת");
   const user: IUser = new User({ username: username, email: email, password: password });
   return await user
     .save()
@@ -35,4 +37,11 @@ const login = async (username: string, password: string): Promise<DBResponse> =>
   return new DBResponse(true, getUserJWT(user));
 };
 
-export { register, login };
+const jwtLogin = async (token: string): Promise<Boolean> => {
+  const uid = getPayload(token);
+  const user = await User.findById(uid);
+  if (user) return true;
+  return false;
+};
+
+export { register, login, jwtLogin };
