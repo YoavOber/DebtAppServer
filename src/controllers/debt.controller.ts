f164da2277d57a9db9a44ccacfc1beb0eea5f0bf
YoavOber <yoavoberko@gmail.com>
2022-05-13 12:56:11 +0300

    fixed by generating auto mongoDB created_at and updated_at fields

--- src/controllers/debt.controller.ts
+++ src/controllers/debt.controller.ts
@@ -1,12 +1,10 @@
 import { Response, Request } from "express";
-import mongoose from "mongoose";
 import { create } from "../repository/debt.repository";
 class DebtController {
   async createDebt(req: Request, res: Response) {
     try {
-      const { amount, date, creditors, debitors, reason } = req.body;
-      const dateObject = new mongoose.Schema.Types.Date(date);
-      const result = await create(amount, dateObject, creditors, debitors, reason);
+      const { amount, creditors, debitors, reason } = req.body;
+      const result = await create(amount, creditors, debitors, reason);
       return res.status(result.success ? 200 : 400).send(result.data);
     } catch (error: Error | any) {
       const errMsg: string = error instanceof Error ? error.message : String(error);
